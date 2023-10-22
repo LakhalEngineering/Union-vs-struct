a1efa6dde70c9d011e196f165b35f41c6aae21a9
LakhalEngineering <125914042+LakhalEngineering@users.noreply.github.com>
2023-10-22 20:45:55 +0200

    Update main.c
    union don't save the value of data !!

--- main.c
+++ main.c
@@ -15,7 +15,18 @@ typedef union {
 } myunion;
 
 int main(){
+    mystr s;
+    s.x = 45;
+    s.y = 1.5;
+    s.z = 'J';
+
+    myunion u;
+    u.x = 45;
+    u.y = 6.45;
+    u.z = 'K';
 
     printf("struct size : %lu\n", sizeof(mystr));
+    printf("{%d, %f, %c}\n", s.x, s.y, s.z);
     printf("union size :%lu\n", sizeof(myunion));
+    printf("{%d, %f, %c}\n", u.x, u.y, u.z);
 }
